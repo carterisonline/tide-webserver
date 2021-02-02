40881ea0f444553a7a2e10eb1159c6263c10cfc8
carterisonline <carter2k4@protonmail.com>
2021-02-02 16:47:57 +0000

    Exported global console to the preloader

--- src/preloader.rs
+++ src/preloader.rs
@@ -1,5 +1,6 @@
 use once_cell::sync::Lazy;
 use std::env;
+use crate::Console;
 
 pub static WORKDIR: Lazy<String> = Lazy::new(|| {
     let out = env::var("WORKDIR").expect("Please provide a WORKDIR");
@@ -13,6 +14,10 @@ pub static WORKDIR: Lazy<String> = Lazy::new(|| {
 pub static INDEX: Lazy<String> = Lazy::new(|| {
     std::fs::read_to_string(format!("{}web/index.html", WORKDIR.as_str()))
         .expect("Couldn\'t load `index.html`")
+});
+
+pub static CONSOLE: Lazy<Console> = Lazy::new(|| {
+    Console::new()
 });
 
 pub static ADDR: &'static str = "127.0.0.0:3000";
