c5c061f3a12120610557c0bc256d854451f93157
carterisonline <carter2k4@protonmail.com>
2021-02-22 17:02:39 +0000

    Started working on Post structure on frontend

---
+++ web/components/Post.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <p>{{ greeting }} World!</p>
+  <button v-on:click="printProp">Print Message</button>
+</div>
+</template>
+
+<script>
+module.exports = {
+  props: {
+    greeting: String,
+  },
+  data() {
+    return {
+      greeting: String,
+    };
+  },
+
+  methods: {
+    printProp() {
+      console.log(this.greeting);
+    },
+  },
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
